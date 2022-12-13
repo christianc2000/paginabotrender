d6053527c74239b07d26533d00e62b15c2fa05a5
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2022-12-13 09:42:14 -0400

    prueba-notificaciones

--- helpers/controllerDialogFlow.js
+++ helpers/controllerDialogFlow.js
@@ -509,6 +509,11 @@ const ApiFacebook = async (facebookId) => {
             posicion: 1
         });
 
+        let titulop = "";
+        titulop = `Un nuevo prospecto está registrado`
+        pusher.trigger("actualizar-channel", "actualizar-event", {
+            titulo: titulop,
+        });
     } else {
         const entrada = await Ingreso.findOne({
             prospecto: usuario._id,
